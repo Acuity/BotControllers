a92b7a4b3e4832e9a94ba58b53d8b93e23ddd834
Zach <zgherridge@gmail.com>
2017-09-27 06:59:24 -0500

    World selector.

--- acuity-control-client/src/main/java/com/acuity/control/client/world/WorldManager.java
+++ acuity-control-client/src/main/java/com/acuity/control/client/world/WorldManager.java
@@ -5,14 +5,12 @@ import com.acuity.common.world_data_parser.WorldDataResult;
 import com.acuity.control.client.BotControl;
 import com.acuity.db.domain.common.world.RSWorldSelector;
 import com.acuity.db.domain.vertex.impl.scripts.selector.ScriptNode;
-import com.google.common.collect.Sets;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.time.LocalDateTime;
 import java.util.List;
-import java.util.Map;
-import java.util.function.Predicate;
+import java.util.concurrent.ThreadLocalRandom;
 import java.util.stream.Collectors;
 
 /**
@@ -24,18 +22,18 @@ public class WorldManager {
 
     private BotControl botControl;
     private LocalDateTime lastCheck = LocalDateTime.MIN;
-    private static Predicate<WorldData> predicate = entry -> !entry.isMembers();
 
     public WorldManager(BotControl botControl) {
         this.botControl = botControl;
     }
-
 
     public boolean onLoop(){
         if (!botControl.isSignedIn()) return false;
 
         RSWorldSelector rsWorldSelector = botControl.getBotClientConfig().getScriptSelector().getRsWorldSelector();
         rsWorldSelector = botControl.getScriptManager().getExecutionNode().map(ScriptNode::getRsWorldSelector).orElse(rsWorldSelector);
+
+        if (rsWorldSelector != null && !rsWorldSelector.isEnabled()) return false;
 
         Integer currentWorld = botControl.getCurrentWorld();
         if (currentWorld == null) return false;
@@ -44,28 +42,31 @@ public class WorldManager {
             WorldDataResult worldData = botControl.requestWorldData();
             worldData.zip();
 
-            int currentWorldBotPopulation = worldData.getWorldBotPopulation().getOrDefault(currentWorld, Integer.MAX_VALUE) - 1;
+            int currentWorldBotPopulation = worldData.getWorldBotPopulation().getOrDefault(currentWorld, Integer.MAX_VALUE) - 2;
 
             RSWorldSelector finalRsWorldSelector = rsWorldSelector;
+
             List<WorldData> betterWorlds = worldData.getWorldData().stream()
                     .filter(entry -> entry.getBotPopulation() < currentWorldBotPopulation)
                     .filter(entry -> {
-                        if (finalRsWorldSelector == null || !finalRsWorldSelector.isEnabled()) return true;
+                        if (finalRsWorldSelector == null) return true;
 
                         if (entry.isMembers() && !finalRsWorldSelector.isP2p()) return false;
                         if (!entry.isMembers() && !finalRsWorldSelector.isF2p()) return false;
 
                         return true;
                     })
-                    .sorted((o1, o2) -> Double.compare(o2.getPopulation(), o1.getPopulation()))
+                    .sorted((o1, o2) -> Double.compare(o1.getPopulation(), o2.getPopulation()))
                     .collect(Collectors.toList());
 
             logger.info("Viable worlds vs current pop. {}, {}", currentWorldBotPopulation, betterWorlds);
 
             if (betterWorlds.size() > 0){
-                int world = betterWorlds.get(0).getWorld();
+                int world = betterWorlds.get(ThreadLocalRandom.current().nextInt(0, Math.min(5, betterWorlds.size()))).getWorld();
                 logger.info("Found better world. {}",world );
                 botControl.hopToWorld(world);
+                lastCheck = LocalDateTime.now().plusMinutes(1);
+                return true;
             }
 
             lastCheck = LocalDateTime.now();
