67784726b98f73ee8a367a4f010cb0ec8a802b11
Zach <zgherridge@gmail.com>
2017-10-13 11:13:23 -0500

    Fixes.

--- acuity-control-launcher/src/main/java/com/acuity/control/launcher/Bootstrap.java
+++ acuity-control-launcher/src/main/java/com/acuity/control/launcher/Bootstrap.java
@@ -15,17 +15,19 @@ public class Bootstrap {
     private static final Logger logger = LoggerFactory.getLogger(Bootstrap.class);
 
     public static void main(String[] args) {
+        args = new String[]{"Dreambot", "1"};
+
         String clientType = args[0];
         String count = args[1];
 
-        logger.info("Launching Acuity with args. {}, {}.", clientType, count);
+        logger.info("Launching Acuity with args. '{}', '{}'.", clientType, count);
 
         String[] quickstart = Arrays.copyOfRange(args, 1, args.length - 1);
         logger.debug("Quickstart args. {}", Arrays.toString(quickstart));
 
         if (clientType.equalsIgnoreCase("Dreambot")){
             try {
-                Launcher.updateVersion("dreambotController", "dreambotContollerVersion", new File(AcuityDir.getHome(), "dreambotController"));
+                Launcher.updateVersion("dreambotController", "dreambotContollerVersion", new File(AcuityDir.getHome(), "dreambotController.jar"));
             } catch (Throwable e) {
                 logger.error("Error during updating dreambot controller.", e);
             }
--- acuity-control-launcher/src/main/java/com/acuity/control/launcher/Launcher.java
+++ acuity-control-launcher/src/main/java/com/acuity/control/launcher/Launcher.java
@@ -3,6 +3,7 @@ package com.acuity.control.launcher;
 import com.acuity.common.util.AcuityDir;
 import com.acuity.common.util.ControlUtil;
 import com.acuity.common.util.DownloadUtil;
+import com.google.gson.internal.LinkedTreeMap;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -19,9 +20,9 @@ public class Launcher {
 
     @SuppressWarnings("unchecked")
     public static void updateVersion(String globalKey, String propertiesKey, File file) throws IOException {
-        HashMap<String, Object> globalInfoDoc = ControlUtil.getGlobalInfoDoc();
+        HashMap<String, Object> globalInfoDoc = ControlUtil.getGlobalInfoDoc("http://174.53.192.24/Control/GlobalInfo");
 
-        HashMap<String, Object> versionInfo = (HashMap<String, Object>) globalInfoDoc.get(globalKey);
+        LinkedTreeMap<String, Object> versionInfo = (LinkedTreeMap<String, Object>) globalInfoDoc.get(globalKey);
 
         String globalVersion = String.valueOf(versionInfo.get("version"));
         String propertyVersion = AcuityDir.getProperties().getProperty(propertiesKey);
@@ -29,8 +30,10 @@ public class Launcher {
         logger.info("Comparing versions. remote={}, local={}", globalVersion, propertyVersion);
 
         if (propertyVersion == null || !propertyVersion.equalsIgnoreCase(globalVersion)){
-            String url = String.valueOf(globalInfoDoc.get("url"));
+            String url = String.valueOf(versionInfo.get("url"));
+            logger.info("Downloading new version. {}", url);
             DownloadUtil.download(url, file.getPath());
+            AcuityDir.setProperty(propertiesKey, globalVersion);
         }
     }
 
